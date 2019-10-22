d23b20c89c8bcf16ff95d28178670eef74ea5f82
julivega99 <juliana.vega_del_castillo@tufts.edu>
2019-10-21 20:09:32 -0400

    finished

--- hw6/main.js
+++ hw6/main.js
@@ -11,11 +11,11 @@ function createArray () {
 
 	arr = new Array();
 
-	var a = new Art( "Starry Night", "priceless", "will1.jpg", "will1.jpg");
-	var b = new Art( "Starry Night", "priceless", "will2.jpg", "will2.jpg");
-	var c = new Art( "Starry Night", "priceless", "will3.jpg", "will3.jpg");
-	var d = new Art( "Starry Night", "priceless", "will4.jpg", "will4.jpg");
-	var e = new Art( "Starry Night", "priceless", "will5.jpg", "will5.jpg");
+	var a = new Art( "A quick guitar tune before class", "$1", "will1.jpg", "will1.jpg");
+	var b = new Art( "A smile to warm your day", "Priceless", "will2.jpg", "will2.jpg");
+	var c = new Art( "When you have not finished 105", "$105", "will3.jpg", "will3.jpg");
+	var d = new Art( "Hover did not work", "$20", "will4.jpg", "will4.jpg");
+	var e = new Art( "Ryan, can we bike?", "$13", "will5.jpg", "will5.jpg");
 
 	arr.push(a);
 	arr.push(b);
@@ -36,8 +36,16 @@ $("#smallImages span").hover ( function() {
 	var i = parseInt(this.id[3]) - 1;
 	$('#big').html("<img class='bigImage' src='" + arr[i].large +"'>");
 	}, function () {
-		$('#big').html('');
+		$('#big').html("<div class='title' id='title'> Will, </br> multifaceted </div>");
 	}
+);
+
+$("#smallImages span").click ( function() {
+	var i = parseInt(this.id[3]) - 1;
+	$("#text").html("'" + arr[i].name + "'" + "</br>" + arr[i].price);
+	$("#overlay").show().delay(5000).fadeOut();
+
+}
 );
 
 showCase();
